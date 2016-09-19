c84eae081bc64576618dffe6ec1aae2d781ee01c
valdman <boris.valdman@live.ru>
2016-09-19 16:00:21 +0300

    "Complex", "Rational" and .gitignore added

---
+++ valdman_b_s/prj/complex/complex.cpp
@@ -0,0 +1,156 @@
+#include <iostream>
+#include <sstream>
+
+struct Complex {
+	Complex() {}
+	
+	explicit Complex(const double real);
+	Complex(const double real, const double imaginary);
+
+	bool operator==(const Complex& rhs) const { return (re == rhs.re) && (im == rhs.im); }
+	bool operator!=(const Complex& rhs) const { return !operator==(rhs); }
+
+	Complex& operator+=(const Complex& rhs)
+	{
+		re += rhs.re;
+		im += rhs.im;
+		return *this;
+	}
+
+	Complex& operator+=(const double rhs) 
+	{ 
+		return operator+=(Complex(rhs)); 
+	}
+
+	Complex& operator-=(const Complex& rhs) 
+	{
+		re -= rhs.re;
+		im -= rhs.im;
+		return *this;
+	}
+
+	Complex& operator-=(const double rhs) 
+	{ 
+		return operator-=(Complex(rhs)); 
+	}
+
+	Complex& operator*=(const Complex& rhs)
+	{
+		re = re * rhs.re - im * rhs.im;
+		im = re * rhs.im + im * rhs.re;
+		return *this;
+	};
+
+	Complex& operator*=(const double rhs)
+	{
+		re *= rhs;
+		im *= rhs;
+		return *this;
+	};
+
+	std::ostream& writeTo(std::ostream& ostrm) const;
+	std::istream& readFrom(std::istream& istrm);
+
+	double re{ 0.0 };
+	double im{ 0.0 };
+	static const char leftBrace{ '{' };
+	static const char separator{ ',' };
+	static const char rightBrace{ '}' };
+};
+
+Complex operator+(const Complex& lhs, const Complex& rhs)
+{
+	Complex answer(lhs);
+	return answer += rhs;
+};
+
+Complex operator-(const Complex& lhs, const Complex& rhs) 
+{
+	Complex answer(lhs);
+	return answer -= rhs;
+};
+
+Complex operator*(const Complex& lhs, const Complex& rhs)
+{
+	Complex answer(lhs);
+	return answer *= rhs;
+};
+
+inline std::ostream& operator<<(std::ostream& ostrm, const Complex& rhs)
+{
+	return rhs.writeTo(ostrm);
+}
+
+inline std::istream& operator >> (std::istream& istrm, Complex& rhs)
+{
+	return rhs.readFrom(istrm);
+}
+
+bool testParse(const std::string& str)
+{
+	using namespace std;
+	istringstream istrm(str);
+	Complex z;
+	istrm >> z;
+	if (istrm.good()) {
+		cout << "Read success: " << str << " -> " << z << endl;
+	}
+	else {
+		cout << "Read error : " << str << " -> " << z << endl;
+	}
+	return istrm.good();
+}
+
+int main()
+{
+	using namespace std;
+
+	Complex z;
+	z += Complex(8.0);
+	testParse("{8.9,9}");
+	testParse("{8.9, 9}");
+	testParse("{8.9,9");
+
+	system("pause");
+	return 0;
+}
+
+Complex::Complex(const double real)
+	: Complex(real, 0.0)
+{
+}
+
+Complex::Complex(const double real, const double imaginary)
+: re(real)
+, im(imaginary)
+ { }
+
+std::ostream& Complex::writeTo(std::ostream& ostrm) const
+{
+	ostrm << leftBrace << re << separator << im << rightBrace;
+	return ostrm;
+}
+
+std::istream& Complex::readFrom(std::istream& istrm)
+{
+	char leftBrace(0);
+	double real(0.0);
+	char comma(0);
+	double imaganary(0.0);
+	char rightBrace(0);
+	istrm >> leftBrace >> real >> comma >> imaganary >> rightBrace;
+	if (istrm.good()) {
+		if ((Complex::leftBrace == leftBrace) && (Complex::separator == comma)
+			&& (Complex::rightBrace == rightBrace)) {
+			re = real;
+			im = imaganary;
+
+		}
+		else {
+			istrm.setstate(std::ios_base::failbit);
+
+		}
+
+	}
+	return istrm;
+}
---
+++ valdman_b_s/prj/rational/rational.cpp
@@ -0,0 +1,170 @@
+#include <iostream>
+#include <cstdint>
+
+class Rational
+{
+public:
+	Rational() {};
+
+	Rational(const int number) : _divided{ number } {}
+
+	Rational(const int divided, const int dividor) : _divided{divided}, _dividor{dividor}
+	{
+		if (!dividor)
+			; //throw new Exception();
+
+		_normalize();
+	}
+
+public: //overriding operators
+
+	Rational& operator+=(const Rational& value)
+	{
+		_divided = _divided * value._dividor + _dividor * value._divided;
+		_dividor *= value._dividor;
+		_normalize();
+		return *this;
+	}
+
+	Rational& operator-=(const Rational& value)
+	{
+		_divided = _divided * value._dividor - _dividor * value._divided;
+		_dividor *= value._dividor;
+		_normalize();
+		return *this;
+	}
+
+	Rational& operator/=(const Rational& value)
+	{
+		_divided *= value._dividor;
+		_dividor *= value._divided;
+		_normalize();
+		return *this;
+	}
+
+	Rational& operator*=(const Rational& value)
+	{
+		_divided *= value._divided;
+		_dividor *= value._dividor;
+		_normalize();
+		return *this;
+	}
+
+	bool IsPositive() 
+	{
+		return _divided > 0 == _dividor > 0;
+	}
+
+	bool IsZero()
+	{
+		return _divided == 0;
+	}
+
+private:
+	int32_t _divided{ 0 };
+	int32_t _dividor{ 1 };
+
+	int32_t gcd(const int32_t a, const int32_t b)
+	{
+		int firstNumber = a;
+		int secondNumber = b;
+
+		int c;
+		while (secondNumber)
+		{
+			c = firstNumber % secondNumber;
+			firstNumber = secondNumber;
+			secondNumber = c;
+		}
+
+		return(abs(firstNumber));
+	}
+
+	void _normalize()
+	{
+		int32_t nod = gcd(_divided, _dividor);
+		_divided /= nod;
+		_dividor /= nod;
+	}
+};
+
+Rational operator+(const Rational& left, const Rational& right)
+{
+	Rational answer{ left };
+
+	answer += right;
+	return answer;
+}
+
+Rational operator-(const Rational& left, const Rational& right)
+{
+	Rational answer{ left };
+
+	answer -= right;
+	return answer;
+}
+
+Rational operator*(const Rational& left, const Rational& right)
+{
+	Rational answer{ left };
+
+	answer *= right;
+	return answer;
+}
+
+Rational operator/(const Rational& left, const Rational& right)
+{
+	Rational answer{ left };
+
+	answer /= right;
+	return answer;
+}
+
+bool operator>(const Rational& left, const Rational& right)
+{
+	Rational tmp{ left };
+
+	tmp -= right;
+	return tmp.IsPositive;
+}
+
+bool operator<(const Rational& left, const Rational& right)
+{
+	Rational tmp{ left };
+
+	tmp -= right;
+	return !tmp.IsPositive;
+}
+
+bool operator>=(const Rational& left, const Rational& right)
+{
+	return !(left < right);
+}
+
+bool operator<=(const Rational& left, const Rational& right)
+{
+	return !(left > right);
+}
+
+
+int main() 
+{
+	Rational firstNumber{ 5, 2 };
+	Rational secondNumber{ 7, 8 };
+	Rational zero{0};
+
+	Rational sum = firstNumber + secondNumber;
+	Rational partial = firstNumber - secondNumber;
+
+	Rational multiply = firstNumber * secondNumber;
+	Rational divide = firstNumber / secondNumber;
+
+	Rational sumInt = secondNumber + 3;
+	Rational partialInt = secondNumber - 2;
+	Rational multiplyInt = firstNumber * 2;
+	Rational divideInt = firstNumber / 2;
+
+	firstNumber += secondNumber;
+
+	system("pause");
+}
