77241024d426a73213b59d5b03b0a85c98651d4b
valdman <boris.valdman@live.ru>
2016-10-17 15:03:04 +0300

    CMakeLists added in nod

--- valdman_b_s/prj/nod/nod.cpp
+++ valdman_b_s/prj/nod/nod.cpp
@@ -20,8 +20,8 @@ int gcd(int a, int b)
 
 int main()
 {
-    cout << gcd(144, 60);
-    cout << gcd(77, 77);
-    cout << gcd(-11, 77);
-    cout << gcd(-12, -4);
+    cout << "gcd(144, 60) = " << gcd(144, 60)  << endl;
+    cout << "gcd(77, 77) = " << gcd(77, 77)<< endl;
+    cout << "gcd(-11, 77) = "  << gcd(-11, 77) << endl;
+    cout << "gcd(-12, -4) = " << gcd(-12, -4) << endl;
 }
